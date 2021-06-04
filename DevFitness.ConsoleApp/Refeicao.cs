b24c17cb5cda23d8bce3427b501b22a2769f99bc
Sara-Alcaras <saraalcaras401@gmail.com>
2021-06-04 17:17:18 -0300

    Cadastro, listagem e detalhes

---
+++ DevFitness.ConsoleApp/Refeicao.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFitness.ConsoleApp
+{
+    public class Refeicao
+
+    {
+        public Refeicao(string descricao, int calorias)
+        {
+            Descricao = descricao;
+            Calorias = calorias;
+        }
+
+        public string Descricao { get; private set; } 
+        public int Calorias { get; private set; }
+
+        public virtual void ImprimirMensagem()
+        {
+            Console.WriteLine($"{Descricao}, com {Calorias} calorias.");
+        }
+
+    }
+}
+
+// public: acesso é público
+// protected: o acesso está limitado para própria classe ou para classes derivadas
+// internal: o acesso está limitado ao assembly atual
+// private: o acesso está limitado a própria classe
